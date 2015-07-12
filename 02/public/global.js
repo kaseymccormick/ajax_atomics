580b11f89809273628e0699ab296504836895ac9
Kasey McCormick <mccormick.kasey@gmail.com>
2015-07-12 17:46:29 -0500

    completed atomic 2

--- 02/public/global.js
+++ 02/public/global.js
@@ -1,14 +1,19 @@
 var req = new XMLHttpRequest();
 
 // For 'Exercise 2', you will modify this line:
-req.open("get", "/path1");
+req.open("get", "/path2");
 
 req.addEventListener("loadstart", function(){
   console.log("loadstart");
 });
 
 req.addEventListener("load", function(){
+  alert(this.response)
   // Your code for Exercise 1 goes here.
+  if (this.response === "yes"){
+    alert("yay!");
+  } else 
+  alert("Aw, shucks!");  
 });
 
 req.send();
