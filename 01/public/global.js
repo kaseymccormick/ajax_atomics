420cd629784e7e8227e72edadef49d589a1c21f8
Kasey McCormick <mccormick.kasey@gmail.com>
2015-07-12 17:27:00 -0500

    initial commit

---
+++ 01/public/global.js
@@ -0,0 +1,16 @@
+var req = new XMLHttpRequest();
+
+// For 'Exercise 2', you will modify this line:
+req.open("get", "/info1");
+
+req.addEventListener("loadstart", function(){
+  console.log("loadstart");
+});
+
+req.addEventListener("load", function(){
+  // Your code for Exercise 1 goes here.
+  alert(this.response);  
+  console.log(this.response);
+});
+
+req.send();
