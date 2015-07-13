8231289ece457195675fa87edc885f7afe879582
Kasey McCormick <mccormick.kasey@gmail.com>
2015-07-12 20:10:15 -0500

    Project done

--- 04/app.rb
+++ 04/app.rb
@@ -9,13 +9,13 @@ get "/:msg" do
   first_char = message[0]
   
   if ('A'..'Z').include?(first_char)
-    return "You wrote #{message}, is that your name?"
+    @response_message = "You wrote #{message}, is that your name?"
     
   elsif ("0".."9").include?(first_char)
-    return "You wrote #{message}, is that how old you are?"
+    @response_message = "You wrote #{message}, is that how old you are?"
     
   else  
-    return "You wrote #{message}. Whatever"
+    @response_message = "You wrote #{message}. Whatever"
   end
 
 end
