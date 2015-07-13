550d3031edb457c3d4526ac95c0c52c67e30dabd
Kasey McCormick <mccormick.kasey@gmail.com>
2015-07-12 21:28:51 -0500

    trying to just make the loop without looking at steps

--- 05/sinatra/public/global.js
+++ 05/sinatra/public/global.js
@@ -2,4 +2,22 @@ var finishLinks = document.getElementsByClassName("done_link");
 
 for (var i=0; i < finishLinks.length; i++) {
   // finishLinks[i] represents each of the items in `finishLinks`.
+  //loop above created, needs to acheive -> marks task as finished
+  var task = document.getElementById("/tasks/mark_as_done/<%= task.id %>")
+  var tagContent = document.getElementsByTagName("a")[0].textContent; (returns mark done)
+  //
+  // function myFunction() {
+  //     var x = document.getElementsByTagName("BUTTON")[0].textContent;
+  //     document.getElementById("demo").innerHTML = x;  
+  //}
 }
+alert(tagContent)
+function finished(){
+  document.getElementById("").className = "task finished"
+}
+
+when click on mark done
+1
+2
+3
+4
